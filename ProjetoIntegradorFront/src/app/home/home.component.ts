7ce6f81e91dc5da7158f4c120be3b662f7b650b2
Edgard Henrique <edigarhenriqu@gmail.com>
2022-07-14 16:57:14 -0300

    Cards home

--- ProjetoIntegradorFront/src/app/home/home.component.ts
+++ ProjetoIntegradorFront/src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Categoria } from '../model/Categoria';
+import { CategoriasService } from '../service/categorias.service';
 
 @Component({
   selector: 'app-home',
@@ -8,16 +10,27 @@ import { environment } from 'src/environments/environment.prod';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  massas: Categoria = new Categoria()
+  listaCategorias: Categoria[];
 
   constructor(
-    private router: Router
+    private router: Router,
+    private categoriaService: CategoriasService,
   ) { }
 
   ngOnInit(){
 
+  
     if(environment.token == ''){
       this.router.navigate(['/login'])
     }
+    
   }
 
+  GetCategoriasById(id: number){
+    this.categoriaService.getByIdCategorias(id).subscribe((resp: Categoria) => {
+      this.massas = resp
+  })
+
+}
 }
